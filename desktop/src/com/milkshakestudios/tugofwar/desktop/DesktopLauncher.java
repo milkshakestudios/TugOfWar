8036669c6e05e2abd941613ef86965bca18eb624
milkshakestudios <milkshakestudios1@gmail.com>
2017-02-23 18:15:48 -0500

    Initial commit

---
+++ desktop/src/com/milkshakestudios/tugofwar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.milkshakestudios.tugofwar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.milkshakestudios.tugofwar.TugOfWarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TugOfWarGame(), config);
+	}
+}
