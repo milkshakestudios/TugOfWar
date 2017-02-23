dfa64a3f2cde359e5e249275d15eff409e0a0aa4
milkshakestudios <milkshakestudios1@gmail.com>
2017-02-23 18:24:13 -0500

    change main class

--- core/src/com/milkshakestudios/tugofwar/TugOfWarGame.java
+++ core/src/com/milkshakestudios/tugofwar/TugOfWarGame.java
@@ -1,33 +1,47 @@
 package com.milkshakestudios.tugofwar;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 
-public class TugOfWarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class TugOfWarGame extends Game {
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void dispose() {
+		super.dispose();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void pause() {
+		super.pause();
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resume() {
+		super.resume();
+	}
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+	@Override
+	public void create() {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+
+	@Override
+	public void setScreen(Screen screen) {
+		super.setScreen(screen);
+	}
+
+	@Override
+	public Screen getScreen() {
+		return super.getScreen();
 	}
 }
